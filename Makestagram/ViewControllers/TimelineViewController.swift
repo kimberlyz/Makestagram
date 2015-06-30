c6c4c482c42dac012231354d9e010c633e2fcf44
Kimberly Zai <kimberlyz621@gmail.com>
2015-06-30 13:59:26 -0700

    "Created TableViewCells that are able to display the images on the TimelineViewController. However, the image downloads are still running on the main thread."

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -11,13 +11,43 @@ import Parse
 
 class TimelineViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     var photoTakingHelper: PhotoTakingHelper?
+    var posts: [Post] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.tabBarController?.delegate = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
+        
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        query.includeKey("user")
+        query.orderByDescending("createdAt")
+        query.findObjectsInBackgroundWithBlock {(result: [AnyObject]?, error: NSError?) -> Void in
+
+            self.posts = result as? [Post] ?? []
+            
+            for post in self.posts {
+                let data = post.imageFile?.getData()
+                post.image = UIImage(data: data!, scale:1.0)
+            }
+    
+            self.tableView.reloadData()
+        }
     }
     
     func takePhoto() {
@@ -41,6 +71,24 @@ extension TimelineViewController: UITabBarControllerDelegate {
         } else {
             return true
         }
+    }
+    
+}
+
+extension TimelineViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 2
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
+        
+        cell.postImageView.image = posts[indexPath.row].image
+        
+        return cell
     }
     
 }
---
+++ Makestagram/Views/Timeline/PostTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PostTableViewCell.swift
+//  Makestagram
+//
+//  Created by Kimberly Zai on 6/30/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+}
