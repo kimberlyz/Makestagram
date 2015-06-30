dd5897008b45826d36aa4b767df1b0c415289daf
Kimberly Zai <kimberlyz621@gmail.com>
2015-06-30 11:12:53 -0700

    "Uploaded photos to Parse, was able to associate a user with a Post, and ran the image download on a background thread"

---
+++ Makestagram/Models/Post.swift
@@ -0,0 +1,57 @@
+//
+//  Post.swift
+//  Makestagram
+//
+//  Created by Kimberly Zai on 6/30/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class Post : PFObject, PFSubclassing {
+    
+    @NSManaged var imageFile: PFFile?
+    @NSManaged var user: PFUser?
+    
+    var image: UIImage?
+    var photoUploadTask : UIBackgroundTaskIdentifier?
+    
+    // MARK: PFSubclassing Protocol
+    
+    static func parseClassName() -> String {
+        return "Post"
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    override class func initialize() {
+        var onceToken : dispatch_once_t = 0;
+        dispatch_once(&onceToken) {
+            // inform Parse about this subclass
+            self.registerSubclass()
+        }
+    }
+    
+    func uploadPost() {
+        
+        let imageData = UIImageJPEGRepresentation(image, 0.8)
+        let imageFile = PFFile(data: imageData)
+        
+        photoUploadTask = UIApplication.sharedApplication().beginBackgroundTaskWithExpirationHandler { () -> Void in
+            UIApplication.sharedApplication().endBackgroundTask(self.photoUploadTask!)
+        }
+        
+        imageFile.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            // 3
+            UIApplication.sharedApplication().endBackgroundTask(self.photoUploadTask!)
+        }
+        
+        // any uploaded post should be associated with the current user
+        user = PFUser.currentUser()
+        self.imageFile = imageFile
+        saveInBackgroundWithBlock(nil)
+    }
+}
